757e5b386f12e1bb46ea7c421a2c08b9e885358f
sergi <sergi.bonell.7e4@itb.cat>
2022-02-22 20:22:33 +0100

    Added night mode override

--- app/src/main/java/cat/itb/totaplayer/MainActivity.kt
+++ app/src/main/java/cat/itb/totaplayer/MainActivity.kt
@@ -2,10 +2,13 @@ package cat.itb.totaplayer
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // I add this line because I don't have a dark mode and I don't want the app to change
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         setTheme(R.style.Theme_Totaplayer)
         setContentView(R.layout.activity_main)
     }
